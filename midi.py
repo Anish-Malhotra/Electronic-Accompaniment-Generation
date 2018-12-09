69f4f9ef30b21bfee6553900f217426334df751a
anishmalhotra <anishmalhotra97@gmail.com>
2018-12-08 20:46:05 -0500

    Instruments extracted to classifications and event matrices built based on note-on note-off ticks

---
+++ midi.py
@@ -0,0 +1,67 @@
+from __future__ import print_function
+
+import pretty_midi
+import numpy as np
+
+num_classes = 7
+note_shift = 25
+max_note = 77
+
+filename = 'Deadmau5 - Jaded.mid'
+filename2 = 'calvin_harris-feel_so_close.mid' 
+
+pm = pretty_midi.PrettyMIDI(filename)
+
+def getInstrumentClass(instrNumber):
+	if(instrNumber in range(24,48)):
+		return 'String'
+	elif((instrNumber in range(0,8)) or (instrNumber in range(16,24))):
+		return 'Piano'
+	elif((instrNumber in range(8,16)) or (instrNumber in range(112,120))):
+		return 'Percussion'
+	elif(instrNumber in range(56,64)):
+		return 'Brass'
+	elif(instrNumber in range(64,80)):
+		return 'Wind'
+	elif(instrNumber in range(48,56)):
+		return 'Ensemble'
+	else:
+		return 'Ethnic'
+
+def fillNotes(matrix, instrument):
+	note_seq = instrument.notes
+	for note in note_seq:
+		if note.pitch in range(note_shift,max_note+note_shift):
+			start_tick = pm.time_to_tick(note.start)
+			end_tick = pm.time_to_tick(note.end)
+			for tick in range(start_tick, end_tick):
+				matrix[tick][note.pitch - note_shift] = 1
+
+num_instruments = len(pm.instruments)
+num_ticks = pm.time_to_tick(pm.get_end_time())
+
+matrix_strings = np.zeros([num_ticks,max_note])
+matrix_piano = np.zeros([num_ticks,max_note])
+matrix_percussion = np.zeros([num_ticks,max_note])
+matrix_brass = np.zeros([num_ticks,max_note])
+matrix_wind = np.zeros([num_ticks,max_note])
+matrix_ensemble = np.zeros([num_ticks,max_note])
+matrix_ethnic = np.zeros([num_ticks,max_note])
+
+def matrixClass(x):
+    return {
+        'String': matrix_strings,
+        'Piano': matrix_piano,
+        'Percussion': matrix_percussion,
+        'Brass': matrix_brass,
+        'Wind': matrix_wind,
+        'Ensemble': matrix_ensemble,
+        'Ethnic': matrix_ethnic
+    }[x]
+
+for instrument in pm.instruments:
+	matrix = matrixClass(getInstrumentClass(instrument.program))
+	fillNotes(matrix, instrument)
+
+#print(matrix_brass)
+#np.savetxt('output2.txt', matrix_brass)
