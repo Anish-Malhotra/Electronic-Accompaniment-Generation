7563e10af84ecbf3b3a0d3f1181fa658e97a2c3b
Matthew Avallone <mva271@nyu.edu>
2018-12-19 16:56:24 -0500

    added project report

--- note_embedding.py
+++ note_embedding.py
@@ -9,8 +9,7 @@ assert gensim.models.word2vec.FAST_VERSION > -1, "This will be painfully slow ot
 
 # Converts a base36 matrix into a Word2Vec encoded matrix
 def sequenceEncoder(matrix, chords):
-    model = word2vec.Word2Vec(matrix, size=100, window=500, min_count=100,
-                              sample=1e-3, iter=50, workers=cores)
+    model = word2vec.Word2Vec(matrix, size=100, window=500, min_count=100, sample=1e-3, iter=50, workers=cores)
     model.train(chords, total_examples=matrix.shape[0], total_words=1, epochs=10)
 
     fs, temp_path = mkstemp("gensim_temp")  # creates a temp file
