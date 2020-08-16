85b5f2a3e9a3620f223e0c1da19470fa9925e799
Matthew Avallone <mva271@nyu.edu>
2020-08-16 11:31:55 -0400

    added two metrics

--- preprocessing/utils.py
+++ preprocessing/utils.py
@@ -3,7 +3,7 @@ The purpose of this file is to provide functions useful during preprocessing MID
 """
 
 import numpy as np
-import pypianoroll
+from pypianoroll import metrics
 
 def readLabels(instrProgram, instrName, is_drum):
     """
@@ -35,7 +35,7 @@ def readNumbers(instrProgram):
 
 def phrases(instr_pianorolls, phrase_length):
     """
-    Function used to create list of phrases from a list of pianorolls
+    Creates list of phrases from a list of pianorolls
     """
     X_instr_phrases = []
     y_instr_phrases = []
@@ -60,6 +60,101 @@ def phrases(instr_pianorolls, phrase_length):
 
     return X_instr_phrases, y_instr_phrases    
 
+def qualified_note_rate(pianoroll, threshold=2):
+    """
+    Return the ratio of the number of the qualified notes (notes longer than
+    `threshold` (in time step)) to the total number of notes in a pianoroll.
+
+    """
+    if np.issubdtype(pianoroll.dtype, np.bool_):
+        pianoroll = pianoroll.astype(np.uint8)
+    padded = np.pad(pianoroll, ((1, 1), (0, 0)), "constant")
+    diff = np.diff(padded, axis=0).reshape(-1)
+    onsets = (diff > 0).nonzero()[0]
+    offsets = (diff < 0).nonzero()[0]
+    if(len(offsets) == 0):
+        offsets = np.empty(shape=onsets.shape)
+    # print(offsets.shape, offsets)
+    # print(onsets.shape, onsets)
+    # print(threshold)
+    n_qualified_notes = np.count_nonzero(offsets - onsets >= threshold)
+    return n_qualified_notes / len(onsets)
+
+def drum_in_pattern_rate(pianoroll, beat_resolution, tolerance=0.1):
+    """
+    Return the ratio of the number of drum notes that lie on the drum pattern
+    (i.e. at certain time steps) to the total number of drum notes.
+
+    """
+    if beat_resolution not in (4, 6, 8, 9, 12, 16, 18, 24):
+        raise ValueError(
+            "Unsupported beat resolution. Only support 4, 6, 8 ,9, 12, 16, 18 and 42."
+        )
+
+    def _drum_pattern_mask(res, tol):
+        """Return a drum pattern mask with the given tolerance."""
+        if res == 24:
+            drum_pattern_mask = np.tile([1.0, tol, 0.0, 0.0, 0.0, tol], pianoroll.shape[0]//6)
+        elif res == 12:
+            drum_pattern_mask = np.tile([1.0, tol, tol], 4)
+        elif res == 6:
+            drum_pattern_mask = np.tile([1.0, tol, tol], 2)
+        elif res == 18:
+            drum_pattern_mask = np.tile([1.0, tol, 0.0, 0.0, 0.0, tol], 3)
+        elif res == 9:
+            drum_pattern_mask = np.tile([1.0, tol, tol], 3)
+        elif res == 16:
+            drum_pattern_mask = np.tile([1.0, tol, 0.0, tol], 4)
+        elif res == 8:
+            drum_pattern_mask = np.tile([1.0, tol], 4)
+        elif res == 4:
+            drum_pattern_mask = np.tile([1.0, tol], 2)
+        return drum_pattern_mask
+
+    drum_pattern_mask = _drum_pattern_mask(beat_resolution, tolerance)
+    n_in_pattern = np.sum(drum_pattern_mask * np.count_nonzero(pianoroll, 1))
+    return n_in_pattern / np.count_nonzero(pianoroll)
+
+def tonal_distance(pianoroll_1, pianoroll_2, beat_resolution, r1=1.0, r2=1.0, r3=0.5):
+    """Return the tonal distance [1] between the two input pianorolls.
+
+    [1] Christopher Harte, Mark Sandler, and Martin Gasser. Detecting harmonic
+        change in musical audio. In Proc. ACM Workshop on Audio and Music
+        Computing Multimedia, 2006.
+
+    """
+    _validate_pianoroll(pianoroll_1)
+    _validate_pianoroll(pianoroll_2)
+    assert len(pianoroll_1) == len(
+        pianoroll_2
+    ), "Input pianorolls must have the same length."
+
+    def _tonal_matrix(r1, r2, r3):
+        """Return a tonal matrix for computing the tonal distance."""
+        tonal_matrix = np.empty((6, 12))
+        tonal_matrix[0] = r1 * np.sin(np.arange(12) * (7.0 / 6.0) * np.pi)
+        tonal_matrix[1] = r1 * np.cos(np.arange(12) * (7.0 / 6.0) * np.pi)
+        tonal_matrix[2] = r2 * np.sin(np.arange(12) * (3.0 / 2.0) * np.pi)
+        tonal_matrix[3] = r2 * np.cos(np.arange(12) * (3.0 / 2.0) * np.pi)
+        tonal_matrix[4] = r3 * np.sin(np.arange(12) * (2.0 / 3.0) * np.pi)
+        tonal_matrix[5] = r3 * np.cos(np.arange(12) * (2.0 / 3.0) * np.pi)
+        return tonal_matrix
+
+    def _to_tonal_space(pianoroll, tonal_matrix):
+        """
+        Return the tensor in tonal space where chroma features are normalized
+        per beat.
+
+        """
+        beat_chroma = _to_chroma(pianoroll).reshape(-1, beat_resolution, 12)
+        beat_chroma = beat_chroma / np.sum(beat_chroma, 2, keepdims=True)
+        return np.matmul(tonal_matrix, beat_chroma.T).T
+
+    tonal_matrix = _tonal_matrix(r1, r2, r3)
+    mapped_1 = _to_tonal_space(pianoroll_1, tonal_matrix)
+    mapped_2 = _to_tonal_space(pianoroll_2, tonal_matrix)
+    return np.linalg.norm(mapped_1 - mapped_2)
+
 def get_metrics(instr_pianorolls, beat_resolution):
     """
     Metrics used for evaluating MIDI songs
@@ -69,13 +164,10 @@ def get_metrics(instr_pianorolls, beat_resolution):
     for instr_pianoroll in instr_pianorolls:
 
         if np.sum(instr_pianoroll) > 0: # skip emp
-            EB += pypianoroll.metrics.empty_beat_rate(instr_pianoroll, beat_resolution)
-            UPC += pypianoroll.metrics.n_pitches_used(instr_pianoroll)
-            try: # dim errors occur here
-                QN += pypianoroll.metrics.qualified_note_rate(instr_pianoroll)
-                DP += pypianoroll.metrics.drum_in_pattern_rate(instr_pianoroll, beat_resolution)
-            except:
-                continue
+            EB += metrics.empty_beat_rate(instr_pianoroll, beat_resolution)
+            UPC += metrics.n_pitches_used(instr_pianoroll)
+            QN += qualified_note_rate(instr_pianoroll)
+            DP += drum_in_pattern_rate(instr_pianoroll, beat_resolution)
 
     EB = EB / len(instr_pianorolls)
     UPC = UPC / len(instr_pianorolls)
@@ -83,16 +175,3 @@ def get_metrics(instr_pianorolls, beat_resolution):
     DP = DP / len(instr_pianorolls)
 
     return EB, UPC, QN, DP
-
-def get_tonal_distance(instr_pianorolls, melody_pianorolls, beat_resolution):
-    """
-    Metric used for evaluating MIDI songs
-    """
-    TD = 0
-
-    for i in range(0, len(instr_pianorolls)):
-        TD += pypianoroll.metrics.tonal_distance(melody_pianorolls[i], instr_pianorolls[i], beat_resolution)
-
-    TD = int(TD / len(instr_pianorolls))
-
-    return TD
